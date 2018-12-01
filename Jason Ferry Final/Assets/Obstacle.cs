2cd9ce64bbe99a32ee4d808905482944cf637879
JasonFerry <43120122+JasonFerry@users.noreply.github.com>
2018-11-30 20:40:32 -0500

    Finished Game

---
+++ Jason Ferry Final/Assets/Obstacle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    public int damage = 1;
+    public float speed;
+
+    public GameObject effect;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("JaFe_PlayerController"))
+        {
+
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            other.GetComponent<JaFe_PlayerController>().health -= damage;
+            Debug.Log(other.GetComponent<JaFe_PlayerController>().health);
+            Destroy(gameObject);
+        }
+
+    }
+}
